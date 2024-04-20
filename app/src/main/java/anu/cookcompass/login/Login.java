88d3acb08c19c8d43ebbd45de016a77e700d7785
tashiatamara <67252129+tashiatamara@users.noreply.github.com>
2024-04-21 00:14:53 +1000

    :construction: [going] working on the feature: user login

--- app/src/main/java/anu/cookcompass/login/Login.java
+++ app/src/main/java/anu/cookcompass/login/Login.java
@@ -4,24 +4,50 @@ import anu.cookcompass.model.Global;
 import anu.cookcompass.model.User;
 
 public class Login {
-    public static Response login(String username, String password){
-        // TODO: finish this module
+    public static Response login(String username, String password) {
+        // 1. Check username and password format
 
-        // 1. check username and password format
+        /*
+        Regex breakdown:
+        a) (?=.{3}@) : Positive lookbehind assertion making sure there are at least 3 characters before @
+        b) [a-zA-Z0-9]+ : One or more numbers and/or letters
+        c) [a-zA-Z0-9!#$%&*-_=+/]+ : One or more numbers and/or letters and/or special characters
+        d) [a-zA-Z0-9.]+ : One or more numbers and/or letters and/or periods
+         */
 
-        // 2. search user and check if it is null
+        if (username.isEmpty() || username == null || password.isEmpty() || password == null ||
+                username.contains(" ") || password.contains(" ") ||
+                !username.matches("^(?=.{3}@)[a-zA-Z0-9]+[a-zA-Z0-9!#$%&*-_=+/]+@[a-zA-Z0-9.]+$")
+                || username.contains("..")) {
+            return new Response(false, "Wrong username or password format!");
+        }
 
-        // 3. check if password is right
+        // 2. Search user and check if it is null
 
-        // 4. setup current user in global object (global is a singleton object)
-
-        // 5. return useful login message
-
+        //Searches the database for the user with the given username
         User user = Global.getInstance().database.getUserByUsername(username);
-        return new Response(true, "login successful");
+
+        //Check if the username is null or not
+        if (user == null) {
+            return new Response(false, "Username does not exist!");
+        }
+
+        // 3. Check if the password is correct
+        if (user != null && !password.equals(user.password)) {
+            return new Response(false, "Wrong password!");
+        }
+
+        /*
+        4. Assigns the user "user" to the currentUser member variable of the global instance (the Singleton object)
+        of the Global class. In other words: Set "user" as the value of the global instance's currentUser variable.
+         */
+        Global.getInstance().currentUser = user;
+
+        // 5. Return successful login message
+        return new Response(true, "Login successful!");
     }
 
-    public static Response register(String username, String password){
+    public static Response register(String username, String password) {
         // TODO: finish this module
 
         // 1. check username and password format
--- app/src/main/java/anu/cookcompass/login/Response.java
+++ app/src/main/java/anu/cookcompass/login/Response.java
@@ -4,6 +4,7 @@ public class Response {
     public boolean successful;
     public String message;
 
+    //Constructor of the Response object to initialize the "successful" and "message" fields of the object
     public Response(boolean successful, String message){
         this.successful = successful;
         this.message = message;
