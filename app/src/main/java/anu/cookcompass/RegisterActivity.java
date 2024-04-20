78feaf1e5c15c4c0ec9bafb21235e32097b3760d
Jiangbei <u7759982@anu.edu.au>
2024-04-20 16:37:48 +1000

    :recycle: [refactor] simplify the module using Utils function

--- app/src/main/java/anu/cookcompass/RegisterActivity.java
+++ app/src/main/java/anu/cookcompass/RegisterActivity.java
@@ -1,6 +1,5 @@
 package anu.cookcompass;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,13 +8,9 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStreamWriter;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -49,8 +44,7 @@ public class RegisterActivity extends AppCompatActivity {
             if (accountExists) {//check exist, if exist toast
                 Toast.makeText(RegisterActivity.this, "User already exists, please log in directly", Toast.LENGTH_LONG).show();
 
-            }
-            else if (!password1.equals(password2)) {//check
+            } else if (!password1.equals(password2)) {//check
                 Toast.makeText(RegisterActivity.this, "The two password inputs do not match", Toast.LENGTH_LONG).show();
             } else {
                 // Save account to users.json
@@ -63,69 +57,35 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
 
-    //check account exist function TODO: intergrate this into database
+    //check account exist function
     private boolean checkAccountExists(String account) {
-        try {
-            InputStream readUsers = getAssets().open("users.json");
-            String usersJson;
-            // use different methods for different version
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
-                usersJson = new String(readUsers.readAllBytes());
-            } else {
-                ByteArrayOutputStream result = new ByteArrayOutputStream();
-                byte[] buffer = new byte[1024];
-                int length;
-                while ((length = readUsers.read(buffer)) != -1) {
-                    result.write(buffer, 0, length);
-                }
-                usersJson = result.toString("UTF-8");
-            }
-            Gson usersGson = new Gson();
-            //check user existence
-            List<User> userList = usersGson.fromJson(usersJson, new TypeToken<List<User>>() {}.getType());
+        File userFile = Utils.copyFileFromAssets(this, "users.json");
+        List<User> userList = Utils.readJson(userFile, new TypeToken<>() {
+        });
+        if (userList != null) {
             for (User each : userList) {
                 if (each.username.equals(account)) {
                     return true;
                 }
             }
-            return false;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
+        return false;
     }
 
     //save account details into the json file
-    private void saveAccount(String account, String password)  {
-        try {
-            InputStream readUsers = getAssets().open("users.json");
-            String usersJson;
-            // use different methods for different version
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
-                usersJson = new String(readUsers.readAllBytes());
-            } else {
-                ByteArrayOutputStream result = new ByteArrayOutputStream();
-                byte[] buffer = new byte[1024];
-                int length;
-                while ((length = readUsers.read(buffer)) != -1) {
-                    result.write(buffer, 0, length);
-                }
-                usersJson = result.toString("UTF-8");
-            }
-            Gson usersGson = new Gson();
-            List<User> userList = usersGson.fromJson(usersJson, new TypeToken<List<User>>() {}.getType());
-            User newUser=new User();
-            newUser.username=account;
-            newUser.password=password;
-            newUser.collections=new ArrayList<>();
-            userList.add(newUser);
-            String updatedUserJson = usersGson.toJson(userList);
-            // write the new user information into the file
-            OutputStreamWriter writer = new OutputStreamWriter(openFileOutput("users.json", Context.MODE_PRIVATE));
-            writer.write(updatedUserJson);
-            writer.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    private void saveAccount(String account, String password) {
+        File userFile = Utils.copyFileFromAssets(this, "users.json");
+        List<User> userList = Utils.readJson(userFile, new TypeToken<>() {
+        });
+        if (userList == null) {
+            userList = new ArrayList<>();
         }
+        User newUser = new User();
+        newUser.username = account;
+        newUser.password = password;
+        newUser.collections = new ArrayList<>();
+        userList.add(newUser);
+        Utils.saveJson(userFile, userList);
     }
 
 
