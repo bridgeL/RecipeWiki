1b80c3252a26a679fd52cbc4437dd35085ccea09
J-Wen <1262913574@qq.com>
2024-05-05 15:22:12 +1000

    :recycle: [refactor] Modify RegisterActivity and LoginActivity. These two activities could now react on the change of theme.

--- app/src/main/java/anu/cookcompass/LoginActivity.java
+++ app/src/main/java/anu/cookcompass/LoginActivity.java
@@ -1,6 +1,7 @@
 package anu.cookcompass;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -8,6 +9,10 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import anu.cookcompass.broadcast.ThemeUpdateEvent;
 import anu.cookcompass.database.Database;
 import anu.cookcompass.login.Login;
 import anu.cookcompass.model.ThemeConfig;
@@ -58,5 +63,27 @@ public class LoginActivity extends AppCompatActivity {
         notRegisteredTextView.setOnClickListener(v -> {
             Utils.switchPage(this, RegisterActivity.class);
         });
+
+        // register EventBus receiver
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // destroy EventBus receiver
+        EventBus.getDefault().unregister(this);
+    }
+
+    /**
+     * The function that updates the theme color of this page when receiving event from
+     * the EventBus.
+     * @param event The event object from EventBus
+     */
+    @Subscribe
+    public void onMessageEvent(ThemeUpdateEvent event) {
+        // extract color and set theme
+        String color = event.getColorValue();
+        this.getWindow().getDecorView().setBackgroundColor(Color.parseColor(color));
     }
 }
